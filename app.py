d57805576659cf6592a5491d6d6b324d5d659059
Mohammed <mohammedbabatunde2001@gmail.com>
2025-12-27 13:39:11 +0100

    working on streamlit app

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import joblib
 import pandas as pd
 from credit_risk_model.config import MODELS_DIR, DATA_PROCESSED
+import streamlit as st
+import numpy as np
 
 
 model = joblib.load(f"{MODELS_DIR}/LightGBM.joblib")
@@ -14,4 +16,27 @@ df = df[feature_list]
 
 prob = model.predict_proba(df)[0, 1]
 
-print(df, prob)
+st.write(f"Probability of default: {prob}")
+
+st.title("🏦 Credit Risk Scoring App")
+
+st.sidebar.header("Applicant Information")
+
+input_data = {}
+for feat in feature_list:
+    input_data[feat] = st.sidebar.number_input(feat, value=0.0)
+
+df = pd.DataFrame([input_data])
+
+if st.button("Predict Risk"):
+    prob = model.predict_proba(df)[0, 1]
+
+    if prob < 0.2:
+        risk = "Low Risk"
+    elif prob < 0.4:
+        risk = "Medium Risk"
+    else:
+        risk = "High Risk"
+
+    st.metric("Default Probability", f"{prob:.2%}")
+    st.success(f"Risk Category: {risk}")
